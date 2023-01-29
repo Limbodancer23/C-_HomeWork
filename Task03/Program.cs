2afdf3e6d29ad959e9e67b579b09244319e9ac76
Sentinel Magnus <ekko2363@gmail.com>
2023-01-29 22:13:52 +0500

    add task03

---
+++ Task03/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Enter number: ");
+string strnumber1 = Console.ReadLine();
+int number1 = Convert.ToInt32(strnumber1);
+
+if (number1 % 2 == 0) System.Console.WriteLine("It`s even number!");
+else System.Console.WriteLine("It`s odd number!");
