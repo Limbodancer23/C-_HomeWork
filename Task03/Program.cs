c8555f12a82afb144bf96d6faa82bef929717277
Sentinel Magnus <ekko2363@gmail.com>
2023-01-29 23:08:57 +0500

    fixed task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,6 +1,21 @@
-﻿System.Console.WriteLine("Enter number: ");
+﻿System.Console.WriteLine("Enter number:");
 string strnumber1 = Console.ReadLine();
 int number1 = Convert.ToInt32(strnumber1);
 
 if (number1 % 2 == 0) System.Console.WriteLine("It`s even number!");
 else System.Console.WriteLine("It`s odd number!");
+
+int StartNumber = 1;
+while (StartNumber <= number1)
+{
+    if (StartNumber % 2 == 0)
+    {
+        System.Console.Write(StartNumber);
+        System.Console.Write(", ");
+        StartNumber++;
+    }
+    else
+    {
+        StartNumber++;
+    }
+}
